a552b01ea978a48201903917f77e66ea175a01ec
Vianey GarciaE <vg058040@gmail.com>
2022-08-02 19:14:00 -0500

    subir proyecto

---
+++ assets/scrips/script.js
@@ -0,0 +1,23 @@
+ //************ *Dropdown**********
+((d) => {
+    const $btnDesp = d.querySelector(".dropdown-btn"),
+      $DM = d.querySelector(".dropdown-menu ");
+    $services = d.querySelector(".dropdown ");
+  
+         d.addEventListener("mouseup", (e) => {    
+    if (!$DM.contains(e.target)) {
+      if (  !$DM.classList.contains("none")) {
+                $DM.classList.add("none");
+      }
+      if ($btnDesp !=null)
+            $btnDesp.addEventListener("click", (e) => {
+      $DM.classList.toggle("none");
+    });
+      }  
+    });
+  })(document);
+
+  
+
+
+   
